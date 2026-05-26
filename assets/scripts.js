376c62cc26e70b66281c539e0174b39d46d20bc9
pbiggin <pbiggin@student.unimelb.edu.au>
2026-05-26 14:40:46 +1000

    fixed dragging element feature

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,9 +4,9 @@ dragElement(document.getElementById("drag"));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "main")) {
+  if (document.getElementById(elmnt.id + "header")) {
     // identifies main the draggable tab is moved from
-    document.getElementById(elmnt.id + "main").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
@@ -29,11 +29,20 @@ function dragElement(elmnt) {
     // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
     pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    
+
+    // variables for max cursor position:
+    var xMax = window.innerWidth - elmnt.offsetWidth;
+    var yMax = window.innerHeight - elmnt.offsetHeight;
+    // set the element's new position, but locked within the browser size
+    if ((elmnt.offsetLeft - pos1) >= 0 && (elmnt.offsetLeft - pos1) <= xMax) {
+         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+         pos3 = e.clientX;
+    }
+    if ((elmnt.offsetTop - pos2) >= 0 && (elmnt.offsetTop - pos2) <=yMax) { 
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        pos4 = e.clientY
+    }
   }
 
   function closeDragElement() {
